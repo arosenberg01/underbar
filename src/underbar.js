b7d156ba47caafbbf7c186d2c5d2e3a2661113eb
Ansel Rosenberg <ansel01@gmail.com>
2015-02-12 09:20:45 -0800

    Add _.extend

--- src/underbar.js
+++ src/underbar.js
@@ -219,8 +219,16 @@
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
-  };
+   // iterator = iterator || _.identity;
+  
+  // return !_.every(collection, !iterator);
 
+  // return !_.every(collection, function() {
+  //   return !iterator
+      
+  // });
+
+ };
 
   /**
    * OBJECTS
@@ -241,6 +249,13 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    for (var i = 1; i < arguments.length; i++) {
+      for (var key in arguments[i]) {
+        obj[key] = arguments[i][key];
+      }
+    }
+    return obj;
+
   };
 
   // Like extend, but doesn't ever overwrite a key that already
