2954d1c3f7ad4f290ecf107fdb2e4b79196bd800
Ansel Rosenberg <ansel01@gmail.com>
2015-02-11 22:17:52 -0800

    Add _.every

--- src/underbar.js
+++ src/underbar.js
@@ -205,6 +205,14 @@
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    iterator = iterator || _.identity;
+
+    return _.reduce(collection, function(areTrue, item) {
+      if (!areTrue) {
+        return false;
+      }
+      return Boolean(iterator(item));
+    }, true);
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
